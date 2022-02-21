ab75a2daed5dd103ff46bb8a12296d938dbc54bc
Agafitei Bogdan <agafitei_bogdan@yahoo.com>
2022-02-21 15:50:24 +0200

    invoked method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,36 +4,34 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         //car1
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
         Car car1 = new Car();
-        car1.name="Ferrari";
-        car1.mileage=50;
-        car1.fuellevel=60;
-        car1.damaged=true;
-        car1.DoorsNumber=4;
-        System.out.println("Properties of car "+ car1.name);
-        System.out.println("Mileage: "+ car1.mileage);
-        System.out.println("Fuel level : "+ car1.fuellevel);
-        System.out.println("Damaged: "+ car1.damaged);
-        System.out.println("Doors: "+ car1.DoorsNumber);
+        car1.name = "Ferrari";
+        car1.mileage = 50;
+        car1.fuellevel = 60;
+        car1.damaged = true;
+        car1.DoorsNumber = 4;
+        System.out.println("Properties of car " + car1.name);
+        System.out.println("Mileage: " + car1.mileage);
+        System.out.println("Fuel level : " + car1.fuellevel);
+        System.out.println("Damaged: " + car1.damaged);
+        System.out.println("Doors: " + car1.DoorsNumber);
 
         //car2
-        Car car2= new Car();
-        car2.name="Dacia";
-        car2.mileage=50;
-        car2.fuellevel=60;
-        car2.damaged=true;
-        car2.DoorsNumber=4;
-        System.out.println("Properties of car "+ car2.name);
-        System.out.println("Mileage: "+ car2.mileage);
-        System.out.println("Fuel level : "+ car2.fuellevel);
-        System.out.println("Damaged: "+ car2.damaged);
-        System.out.println("Doors: "+ car2.DoorsNumber);
+        Car car2 = new Car();
+        car2.name = "Dacia";
+        car2.mileage = 50;
+        car2.fuellevel = 60;
+        car2.damaged = true;
+        car2.DoorsNumber = 4;
+        System.out.println("Properties of car " + car2.name);
+        System.out.println("Mileage: " + car2.mileage);
+        System.out.println("Fuel level : " + car2.fuellevel);
+        System.out.println("Damaged: " + car2.damaged);
+        System.out.println("Doors: " + car2.DoorsNumber);
 
         //engine1
         Engine engine1 = new Engine();
@@ -41,16 +39,21 @@ public class App
         engine1.capacity = 3000;
         car1.engine = engine1;
 
-        System.out.println("Car1 engine manufacturer " + engine1.manufacturer );
+        System.out.println("Car1 engine manufacturer " + engine1.manufacturer);
         System.out.println("Car1 engine capacity " + engine1.capacity);
 
         //engine2
-        car2.engine=new Engine();
+        car2.engine = new Engine();
         car2.engine.manufacturer = "Bosch";
         car2.engine.capacity = 4000;
 
         System.out.println("Car2 engine manufacturer " + car2.engine.manufacturer);
         System.out.println("Car2 engine capacity " + car2.engine.capacity);
+
+        //invoking the accelerate method
+        double distanceCar1 = car1.accelerate(60, 1);
+
+        double distanceCar2 = car2.accelerate(100, 1);
 
 
     }
