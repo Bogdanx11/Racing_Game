6c3b3b14c022bbae46d03fdd78c12ad7c24f0654
Agafitei Bogdan <agafitei_bogdan@yahoo.com>
2022-02-21 15:37:07 +0200

    added 1 method

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -5,4 +5,23 @@ public class Vehicle {
     double mileage;
     double fuellevel;
     boolean damaged;
+    double totalTraveledDistance;
+
+    // accelerate method
+    public double accelerate(double speed, double timeInHours){
+
+        System.out.println("Vehicle" + name + "is accelerating with " + speed + "for " + timeInHours + "h" );
+
+        double distance = speed * timeInHours;
+
+        totalTraveledDistance += distance;
+        System.out.println("Total traveled distance is " + totalTraveledDistance);
+        damaged = true;
+        System.out.println("Is damaged the car ?" + damaged);
+        double usedFuel = distance * mileage / 100;
+
+        fuellevel -= usedFuel;
+        System.out.println("Remaining fuel level is " + fuellevel);
+        return distance;
+    }
 }
